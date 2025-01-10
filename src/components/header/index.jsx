07ce56e4a6a1228e2c358a40695b474ced94aa27
dohun08 <bemik624@gmail.com>
2025-01-10 19:27:30 +0900

    feat :: 헤더구조

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import * as S from './style.jsx'
+
+export default function Header() {
+    return (
+        <S.HeaderContainer>
+
+        </S.HeaderContainer>
+    )
+}
---
+++ src/components/header/style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+    width: 15%;
+    height: 100%;
+    background-color: #EBF1FF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
