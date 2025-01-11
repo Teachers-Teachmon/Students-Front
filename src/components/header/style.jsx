a9bbda0edfb05f7f39ec4c39baa163bca9533cb6
zuu3 <ojh27467242@gmail.com>
2025-01-11 18:16:52 +0900

    chore :: css 스타일 변경

--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
     position: fixed;
-    width: 13%;
+    width: 13.1%;
     height: 100vh;
     padding: 5% 0;
     background-color: #EBF1FF;
