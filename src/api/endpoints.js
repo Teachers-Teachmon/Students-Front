c9609a9e72fa6a41e57ddd2536357a3e26122512
dohun08 <bemik624@gmail.com>
2025-01-19 22:00:11 +0900

    feat(#30) :: api 틀잡기

---
+++ src/api/endpoints.js
@@ -0,0 +1,7 @@
+export const API_ENDPOINTS = {
+    SUPERVISION : '/supervision',
+    CHANGE:'/change',
+    DATA : '/data',
+    STUDENT:'/student',
+    AFTER_SCHOOL : '/afterschool',
+};
--- src/main.jsx
+++ src/main.jsx
@@ -12,18 +12,23 @@ import After from './pages/after_school'
 import Record from './pages/manage/record'
 import Location from "./pages/manage/location";
 import Login from "./pages/login"
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path={"/login"} element={<Login />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="/manage" element={<Manage />} />
-            <Route path="/manage/record" element={<Record />} />
-            <Route path={"/manage/location"} element={<Location />} />
-            <Route path="/supervision" element={<Supervision />} />
-            <Route path="/after-school" element={<After />} />
-        </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={client} >
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/manage" element={<Manage />} />
+                <Route path="/manage/record" element={<Record />} />
+                <Route path={"/manage/location"} element={<Location />} />
+                <Route path="/supervision" element={<Supervision />} />
+                <Route path="/after-school" element={<After />} />
+            </Routes>
+        </BrowserRouter>
+    </QueryClientProvider>
 )
