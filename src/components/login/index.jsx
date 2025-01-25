3956e77d21e2fb5c62edd1217ee61c1f6a2fbc57
dohun08 <bemik624@gmail.com>
2025-01-25 21:50:23 +0900

    chore :: 로그인 쿠키 삭제 수정

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -9,12 +9,12 @@ export default function LoginLoading(){
             if (parts.length === 2) return parts.pop().split(';').shift();
         }
 
-        // function deleteCookie(name) {
-        //     document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
-        // }
+        function deleteCookie(name) {
+            document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+        }
 
-        localStorage.setItem('accessToken', getCookie('accessToken'));
-        // deleteCookie('accessToken');
+        localStorage.setItem('accessToken', getCookie('access'));
+        deleteCookie('access');
         window.location.href = '/main';
     }, []);
 
