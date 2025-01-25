d78164447878807c391e57882346d19a865643d4
dohun08 <bemik624@gmail.com>
2025-01-25 15:43:22 +0900

    feat(#27) :: 로그인 api 연결작성

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,6 +1,22 @@
-// AT를 받아서 로컬스토리지에 저장해야함, 저장하고 메인으로 보내줘야함
+import {useEffect} from "react";
 
 export default function LoginLoading(){
+
+    useEffect(()=>{
+        function getCookie(name) {
+            const value = `; ${document.cookie}`;
+            const parts = value.split(`; ${name}=`);
+            if (parts.length === 2) return parts.pop().split(';').shift();
+        }
+
+        function deleteCookie(name) {
+            document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+        }
+
+        localStorage.setItem('accessToken', getCookie('accessToken'));
+        deleteCookie('accessToken');
+        window.location.href = '/main';
+    }, []);
 
     return(
         <>
