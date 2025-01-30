7419b42502e2c3b55b7b5ae53362cff3255e8c4a
whdkfk <joara080827@gmail.com>
2025-01-30 17:39:56 +0900

    feat(#32) :: 나의 방과후 조회 추가
    나의 방과후 조회 기능을 추가하였습니다.

--- src/pages/after_school/index.jsx
+++ src/pages/after_school/index.jsx
@@ -3,6 +3,12 @@ import Header from '../../components/header/index.jsx'
 
 export default function After_school() {
 
+    const [options, setOptions] = useState(null);
+    const [isModal1, setIsModal1] = useState(false);
+    const [isModal2, setIsModal2] = useState(false);
+    const [isModal3, setIsModal3] = useState(false);
+    const [selectedClass, setSelectedClass] = useState(null);
+
     const myTodayClasses = [
         {
             time: "8~9",
@@ -17,6 +23,55 @@ export default function After_school() {
             location: "프로그래밍실"
         }
     ];
+
+    const myClasses = [
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "스프링 부트를 이용한 웹서비스 개발",
+            location: "객체지향 프로그래밍실"
+        },
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "파이썬",
+            location: "객체지향 프로그래밍실"
+        },
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "파이썬을 이용한 문제해결",
+            location: "객체지향 프로그래밍실"
+        },
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "파이썬을 이용한 문제해결",
+            location: "프로그래밍실"
+        },
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "파이썬을 이용한 문제해결",
+            location: "프로그래밍실"
+        },
+        {
+            day: "월",
+            time: "8~9교시",
+            grade: "1",
+            name: "파이썬을 이용한 문제해결",
+            location: "프로그래밍실"
+        }
+    ];
+
+    const closeModalHandler = (setModal) => {
+        setModal(false);
+    };
 
     return (
         <S.AfterSchoolContainer>
@@ -34,8 +89,77 @@ export default function After_school() {
                             </S.ClassCard>
                         ))}
                     </S.TodayClasses>
+
+                    <S.MyClasses>
+                        <h1>나의 방과후 수업 ({myClasses.length})</h1>
+                        <S.ClassTableContent>
+                            <S.ClassTableTop>
+                                <span>학년</span>
+                                <span>요일</span>
+                                <span>시간</span>
+                                <S.TopName>이름</S.TopName>
+                                <S.TopLocation>장소</S.TopLocation>
+                            </S.ClassTableTop>
+                            <S.ClassTableMain>
+                                {myClasses.map((cls, i) => (
+                                    <S.ClassTable key={i}>
+                                        <S.TableData $length={46}>{cls.grade}</S.TableData>
+                                        <S.TableData $length={47}>{cls.day}</S.TableData>
+                                        <S.TableData $length={87}>{cls.time}</S.TableData>
+                                        <S.TableData $length={190}>{cls.name}</S.TableData>
+                                        <S.TableData $length={170}>{cls.location}</S.TableData>
+                                        <S.OptionButton src={OptionButton} onClick={() => {
+                                            setOptions(options === i ? null : i);
+                                            setSelectedClass(cls);
+                                        }} />
+                                        {options === i && ( // 옵션 버튼이 클릭된 수업만 목록을 보여줌
+                                            <S.Options onClick={(e) => e.stopPropagation()}>
+                                                <button onClick={() => {
+                                                    setOptions(null);
+                                                    setIsModal1(true);
+                                                }}>출장</button>
+                                                <button onClick={() => {
+                                                    setOptions(null);
+                                                    setIsModal2(true);
+                                                }}>보강</button>
+                                                <button onClick={() => {
+                                                    setOptions(null);
+                                                    setIsModal3(true);
+                                                }}>종료</button>
+                                            </S.Options>
+                                        )}
+                                    </S.ClassTable>
+                                ))}
+                            </S.ClassTableMain>
+                        </S.ClassTableContent>
+                    </S.MyClasses>
                 </S.LeftContainer>
             </S.Content>
+
+            {isModal1 && (
+                <S.BusinessTripModal onClick={() => closeModalHandler(setIsModal1)}>
+                    <S.BusinessModal onClick={(e) => e.stopPropagation()}>
+                        <BusinessTrip closeModal={() => { setIsModal1(false) }} />
+                    </S.BusinessModal>
+                </S.BusinessTripModal>
+            )}
+            {isModal2 && (
+                <S.ClassPrepModal onClick={() => closeModalHandler(setIsModal2)}>
+                    <S.PrepModal onClick={(e) => e.stopPropagation()}>
+                        <ClassPrep closeModal={() => { setIsModal2(false) }} />
+                    </S.PrepModal>
+                </S.ClassPrepModal>
+            )}
+            {isModal3 && (
+                <S.ClassEndModal onClick={() => closeModalHandler(setIsModal3)} >
+                    <S.EndModal onClick={(e) => e.stopPropagation()}>
+                        <ClassEnd
+                            closeModal={() => { setIsModal3(false) }}
+                            selectedClass={selectedClass}
+                        />
+                    </S.EndModal>
+                </S.ClassEndModal>
+            )}
         </S.AfterSchoolContainer>
     )
 }
--- src/pages/after_school/style.jsx
+++ src/pages/after_school/style.jsx
@@ -4,14 +4,14 @@ export const AfterSchoolContainer = styled.div`
     display: flex;
     width: 100%;
     height: 100%;
-`;
+`
 
 export const Content = styled.div`
     display: flex;
     width: 85%;
     height: 100%;
     gap: 50px;
-`;
+`
 
 export const LeftContainer = styled.div`
     display: flex;
@@ -20,7 +20,7 @@ export const LeftContainer = styled.div`
     width: 50%;
     padding: 3%;
     margin-top: 30px;
-`;
+`
 
 export const TodayClasses = styled.div`
     display: flex;
@@ -28,7 +28,7 @@ export const TodayClasses = styled.div`
     gap: 25px;
     padding: 5% 0% 0% 0%;
     width: 50%;
-`;
+`
 
 export const ClassCard = styled.div`
     display: flex;
@@ -40,7 +40,7 @@ export const ClassCard = styled.div`
     height: 60px;
     align-items: center;
     gap: 20px;
-`;
+`
 
 export const CardTime = styled.div`
     background-color: #ECF3FD;
@@ -51,8 +51,101 @@ export const CardTime = styled.div`
     border-radius: 5px;
     justify-content: center;
     align-items: center;
-`;
+`
 
 export const CardData = styled.div`
     width: ${(props) => props.$length}px;
-`;
+`
+
+export const MyClasses = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+`
+
+export const ClassTableContent = styled.div`
+`
+
+export const ClassTableTop = styled.div`
+    display: flex;
+    width: 600px;
+    height: 60px;
+    gap: 20px;
+    border: 1px solid #cccccc;
+    border-top-right-radius: 6px;
+    border-top-left-radius: 6px;
+    align-items: center;
+    padding: 20px;
+    background-color: #EBF1FF;
+
+`
+
+export const ClassTableMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 600px;
+    overflow-y: auto;
+    max-height: 290px;
+`
+
+
+export const TopName = styled.span`
+    padding-left: 40px;
+`
+
+export const TopLocation = styled.span`
+    padding-left: 140px;
+`
+
+export const ClassTable = styled.div`
+    display: flex;
+    padding: 15px 20px;
+    border: 1px solid #cccccc;
+    align-items: center;
+    padding-left: 24px;
+    
+
+    &:last-child {
+        border-bottom-left-radius: 6px;
+        border-bottom-right-radius: 6px;
+    }
+`
+
+export const TableData = styled.div`
+    width: ${(props)=>props.$length}px;
+`
+
+export const OptionButton = styled.img`
+    width: 15px;
+`
+
+export const Options = styled.div`
+    position: absolute;
+    display: flex;
+    border: 1px solid #cccccc;
+    border-radius: 8px;
+    overflow: hidden;
+    background-color: #fff;
+    margin-top: 65px;
+    margin-left: 430px;
+    
+    button {
+        border: 1px;
+        padding: 10px 20px;
+        width: 80px;
+        background-color: #fff;
+        cursor: pointer;
+        border-right: 1px solid #cccccc;
+
+    }
+
+    button:hover {
+        background-color: #f0f0f0;
+    }
+
+    button:last-child {
+        border-right: none; 
+    }
+    
+`
+
