e1020a6ce1e3b2c662fbdcacd11df837b5c8c722
whdkfk <joara080827@gmail.com>
2025-01-30 16:45:59 +0900

    feat(#32) :: 나의 오늘 방과후 조회 추가
    나의 오늘 방과후 조회 기능을 추가하였습니다.

--- src/pages/after_school/index.jsx
+++ src/pages/after_school/index.jsx
@@ -1,10 +1,41 @@
 import * as S from './style.jsx'
 import Header from '../../components/header/index.jsx'
 
-export default function After_school(){
-    return(
-        <>
+export default function After_school() {
+
+    const myTodayClasses = [
+        {
+            time: "8~9",
+            grade: "1학년",
+            name: "리액트",
+            location: "객체지향 프로그래밍실"
+        },
+        {
+            time: "10~11",
+            grade: "1학년",
+            name: "파이썬을 이용한 문제해결",
+            location: "프로그래밍실"
+        }
+    ];
+
+    return (
+        <S.AfterSchoolContainer>
             <Header />
-        </>
+            <S.Content>
+                <S.LeftContainer>
+                    <S.TodayClasses>
+                        <h1>나의 오늘 방과후</h1>
+                        {myTodayClasses.map((cls, i) => (
+                            <S.ClassCard key={i}>
+                                <S.CardTime>{cls.time}</S.CardTime>
+                                <div>{cls.grade}</div>
+                                <S.CardData $length={190}>{cls.name}</S.CardData>
+                                <S.CardData $length={150}>{cls.location}</S.CardData>
+                            </S.ClassCard>
+                        ))}
+                    </S.TodayClasses>
+                </S.LeftContainer>
+            </S.Content>
+        </S.AfterSchoolContainer>
     )
 }
--- src/pages/after_school/style.jsx
+++ src/pages/after_school/style.jsx
@@ -1,1 +1,58 @@
 import styled from "styled-components";
+
+export const AfterSchoolContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    width: 85%;
+    height: 100%;
+    gap: 50px;
+`;
+
+export const LeftContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
+    width: 50%;
+    padding: 3%;
+    margin-top: 30px;
+`;
+
+export const TodayClasses = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    padding: 5% 0% 0% 0%;
+    width: 50%;
+`;
+
+export const ClassCard = styled.div`
+    display: flex;
+    padding: 10px;
+    border: 1px solid #cccccc;
+    border-radius: 6px;
+    background-color: #fff;
+    width: 550px;
+    height: 60px;
+    align-items: center;
+    gap: 20px;
+`;
+
+export const CardTime = styled.div`
+    background-color: #ECF3FD;
+    color: #2E6FF2;
+    display: flex;
+    width: 70px;
+    height: 30px;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CardData = styled.div`
+    width: ${(props) => props.$length}px;
+`;
