f2727a7e01c0d5bd1aba0d5bc6aec9ca1ee4299f
dohun08 <bemik624@gmail.com>
2025-02-04 10:27:59 +0900

    chore :: 문자상수수정

--- src/lib/period.js
+++ src/lib/period.js
@@ -1,5 +1,5 @@
 export const period = {
     "7교시": "SEVEN_PERIOD",
-    "8~9교시": "EIGHT_NINE_PERIOD",
-    "10~11교시": "TEN_ELEVEN_PERIOD"
+    "8~9교시": "EIGHT_AND_NINE_PERIOD",
+    "10~11교시": "TEN_AND_ELEVEN_PERIOD"
 }
