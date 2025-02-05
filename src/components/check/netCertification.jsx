4206c92b0b67a333cd10734a8e65724c682e398c
dohun08 <bemik624@gmail.com>
2025-02-05 18:09:49 +0900

    feat(#183) :: 페이지 접근 설정 수정

--- src/components/check/netCertification.jsx
+++ src/components/check/netCertification.jsx
@@ -1,14 +1,14 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useCheck} from "../../hooks/useAuth.js";
 
-export default function NotCertification(){
+export default function Certification(){
     const {data, isFetching} = useCheck();
-    if (!isFetching && data.data !== "Authentication Success") {
-        return <Outlet /> ;
+    if (!isFetching && data.data === "Authentication Success") {
+        alert('로그인된 사용자는 접근할 수 없습니다.')
+        return <Navigate to="/main" replace />;
     }
-    if(!isFetching){
-        return(
-            <Navigate to="/main" replace />
-        )
-    }
+    if(isFetching) return <></>
+    return(
+        <Outlet />
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -33,11 +33,11 @@ createRoot(document.getElementById('root')).render(
         <BrowserRouter>
             <Routes>
                 <Route path={'/'} element={<Default />} />
-                {/*<Route element={<NotCertification />}> /!* 로그인된 사용자인지 확인 *!/*/}
+                <Route element={<NotCertification />}> {/* 로그인된 사용자인지 확인 */}
                     <Route path="/landing" element={<Landing />} />
                     <Route path={"/login"} element={<Login />} />
                     <Route path={'/oauth'} element={<LoginLoading />} />
-                {/*</Route>*/}
+                </Route>
                 <Route element={<Certification />} >  {/* 로그인된 사용자인지 확인 */}
                     <Route path="/main" element={<Main />} />
                     <Route path="/manage" element={<Manage />} />
