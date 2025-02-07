9f2f56bf356132450ea5bbb04821fa5f561b4144
whdkfk <joara080827@gmail.com>
2025-02-08 00:53:42 +0900

    방과후 수정

---
+++ src/components/modal/arrorModal/index.jsx
@@ -0,0 +1,16 @@
+import * as S from './style.jsx';
+import Error from '../../../assets/Error.svg';
+import Error2 from '../../../assets/Error2.svg';
+
+export default function ArrorModal() {
+
+    return (
+        <S.Wrapper>
+            <S.MainContent>
+                <img src={Error} />
+                <img src={Error2} />
+            </S.MainContent>
+            <h2>//에러메세지</h2>
+        </S.Wrapper>
+    );
+}
