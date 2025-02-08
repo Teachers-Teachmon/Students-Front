1595b383eef5f9f3585c07c2e11f33fca15734cb
zuu3 <ojh27467242@gmail.com>
2025-02-08 16:44:04 +0900

    refactor(#328) :: 자습감독 선생님 검색 수정

--- src/pages/supervision/detail/index.jsx
+++ src/pages/supervision/detail/index.jsx
@@ -95,6 +95,9 @@ export default function SupervisionDetail() {
         <S.Wrapper>
             <Header />
             <S.MainWrap>
+                {Object.values(dropdownOpen).some(status => status) && (
+                    <S.Black onClick={() => setDropdownOpen({})} />
+                )}
                 <S.MainHeader>
                     <h1>자습감독 일정</h1>
                     {!isEditing ? (
--- src/pages/supervision/detail/style.jsx
+++ src/pages/supervision/detail/style.jsx
@@ -126,3 +126,12 @@ export const Modal = styled.div`
     border-radius: 10px;
     padding: 2rem;
 `;
+
+export const Black = styled.div`
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+`
