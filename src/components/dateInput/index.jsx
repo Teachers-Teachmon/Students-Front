251fb9ebb7705aa8be98e0c06084be5890c75155
zuu3 <ojh27467242@gmail.com>
2025-02-10 22:29:51 +0900

    refactor :: 날짜 모달 import

--- src/components/dateInput/index.jsx
+++ src/components/dateInput/index.jsx
@@ -4,7 +4,7 @@ import useDay from "../../zustand/day";
 import patchDay from "../../utils/patchDay.js";
 
 export default function DateInput({ onChange }) {
-    const { setDay, day, today } = useDay();
+    const { setDay, setStart, setEnd, day, today } = useDay();
     const newToday = new Date(patchDay(today));
     const initialDate = `${newToday.getFullYear()}년 ${newToday.getMonth() + 1}월 ${newToday.getDate()}일`;
     const [inputValue, setInputValue] = useState(initialDate);
