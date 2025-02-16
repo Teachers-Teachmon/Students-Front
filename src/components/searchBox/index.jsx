40682a78d9f4f2a6affa7f8dcec416d66f893a63
dohun08 <bemik624@gmail.com>
2025-02-16 23:31:37 +0900

    feat(#465) :: 설명추가

--- src/components/searchBox/index.jsx
+++ src/components/searchBox/index.jsx
@@ -1,6 +1,8 @@
 import * as S from './style.jsx';
 import Search from "../../assets/Search.svg";
-
+// target = placeholder 대상 예 : 선생님, 학생
+// change = 검색값을 바꾸는 함수 예 : setSearch
+// value = 검색값
 export default function SearchBox({target, change, value}) {
     return (
         <S.InputBox>
