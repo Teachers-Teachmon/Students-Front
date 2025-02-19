b7f09edc6baf6998a7430f405091fd4850eabbbf
zuu3 <ojh27467242@gmail.com>
2025-02-19 17:49:42 +0900

    refactor(#468) :: 메인페이지 달력 내부 색 표시 및 일정 표시

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -137,12 +137,11 @@ export default function Main() {
 
     const findSupervisionForDay = (year, month, day) => {
         if (monthlySupervisionData && monthlySupervisionData.length === 0) return null;
-        console.log(monthlySupervisionData.filter(item => item.year === year && item.month === month && item.day === day))
-        return monthlySupervisionData.filter(item => item.year === year && item.month === month && item.day === day);
+        return monthlySupervisionData && monthlySupervisionData.filter(item => item.year === year && item.month === month && item.day === day);
     };
     const findAfterSchoolForDay = (year, month, day) => {
         if (monthlyAfterSchoolData && monthlyAfterSchoolData.length === 0) return null;
-        return monthlyAfterSchoolData.filter(item => item.year === year && item.month === month && item.day === day);
+        return monthlyAfterSchoolData && monthlyAfterSchoolData.filter(item => item.year === year && item.month === month && item.day === day);
     }
     const mapPeriodToLabel = (period) => {
         if (period === "7교시") return "7";
