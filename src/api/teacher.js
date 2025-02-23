091363adf1551ba8e00ee6c81defbaaa110f6f2f
dohun08 <bemik624@gmail.com>
2025-02-24 00:31:04 +0900

    feat(#460) :: 선생님 api 싹 수정

--- src/api/teacher.js
+++ src/api/teacher.js
@@ -16,9 +16,42 @@ export const getTeacher = async (name, order) => {
     }
 }
 
-export const patchTeacher = async ({teachers}) => {
+export const patchTeacher = async (teacher) => {
+    console.log(teacher)
     try {
-        const res = await axiosInstance.post(`${API_ENDPOINTS.TEACHER}`, teachers);
+        const res = await axiosInstance.patch(`${API_ENDPOINTS.TEACHER}/${teacher.teachers.teacher_id}`, {
+            name : teacher.teachers.name,
+            email : teacher.teachers.email,
+            role : teacher.teachers.role
+        });
+        if(res.status!==200 && res.status!==201){
+            return Promise.reject(res.status)
+        }
+        return res;
+    }catch (err){
+        return Promise.reject(err);
+    }
+}
+
+export const deleteTeacher = async (id) => {
+    try {
+        const res = await axiosInstance.delete(`${API_ENDPOINTS.TEACHER}/${id}`);
+        if(res.status!==200){
+            return Promise.reject(res.status)
+        }
+        return res;
+    }catch (err){
+        return Promise.reject(err);
+    }
+}
+
+export const postTeacher = async ({teachers}) => {
+    try {
+        const res = await axiosInstance.post(`${API_ENDPOINTS.TEACHER}`, {
+            name : teachers.name,
+            email : teachers.email,
+            role : teachers.role
+        });
         if(res.status!==200 && res.status!==201){
             return Promise.reject(res.status)
         }
