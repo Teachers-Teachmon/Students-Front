feb6b4b3feb30a14df46f625c30098287467fe0b
dohun08 <bemik624@gmail.com>
2025-02-27 23:41:01 +0900

    chore :: alert 수정

--- src/components/record/student/index.jsx
+++ src/components/record/student/index.jsx
@@ -23,9 +23,9 @@ export default function Student({ data, day }) {
             return newState;
         });
         if(status === "방과후"){
-            // alert("아직 준비가 안됐어여...ㅠ")
-            setIsModal(true);
-            setPeriod(period);
+            alert("아직 준비가 안됐어여...ㅠ 학생을 방과후로 바꾸고 싶으시다면 보강을 이용해주세요!")
+            // setIsModal(true);
+            // setPeriod(period);
             return;
         }
         if (Array.isArray(name)) {
