c0e0e119ad4df347940785e6853de7296e3251ff
dohun08 <bemik624@gmail.com>
2025-03-11 00:30:32 +0900

    feat :: 디테일 반응형 추가

--- src/api/student.js
+++ src/api/student.js
@@ -34,7 +34,7 @@ export const schoolOutStudent = async (studentID) =>{
 
 export const getLocationAll = async ()=>{
     try{
-        const res = await axiosInstance.get(`/location/ALL`);
+        const res = await axiosInstance.get(`${API_ENDPOINTS.STUDENT}/location`);
         if(res.status!==200){
             return new Promise.reject({
                 status:res.status,
@@ -49,7 +49,7 @@ export const getLocationAll = async ()=>{
 
 export const getLocation = async (floor) =>{
     try {
-        const res = await axiosInstance.get(`/location`);
+        const res = await axiosInstance.get(`${API_ENDPOINTS.STUDENT}/location/${floor}`);
         if(res.status!==200){
             return new Promise.reject({
                 status:res.status,
