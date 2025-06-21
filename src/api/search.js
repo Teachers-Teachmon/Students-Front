fe418a0ddc40c8c6f21cfd9fdc7f19939dacc5fd
dohun08 <bemik624@gmail.com>
2025-06-21 20:50:57 +0900

    refactor(#755) :: 엔터시 학생추가, 조직 추가

--- src/api/search.js
+++ src/api/search.js
@@ -4,7 +4,7 @@ import {API_ENDPOINTS} from "../lib/endpoints.js";
 //${API_ENDPOINTS.SEARCH}
 export const searchStudent = async (query) =>{
     try{
-        const res = await axiosInstance.get(`${API_ENDPOINTS.SEARCH}/student?search_query=${query}`);
+        const res = await axiosInstance.get(`/searchStudent?search_query=${query}`);
         if(res.status!==200){
             return new Promise.reject({
                 status:res.status,
